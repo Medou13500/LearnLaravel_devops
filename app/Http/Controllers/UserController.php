310a11323dba3d566519108bbb6374a1e085bb06
Your Name <you@example.com>
2025-05-09 17:15:02 +0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use \App\Models\book;
+Use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public index ()
+
+}
